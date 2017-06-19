21518fe2bf3dc152a3bb5456a749db10155eb163
apekearo <apekearo@gmail.com>
2017-06-19 15:59:48 -0500

    adding media icons

---
+++ crazy.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+
+    $(".bun").addClass("scale-transform")
+    $(".bun1").addClass("scale-transform")
+    $(".hotdog").addClass("scale-transform")
+    $(".cream-cheese-container").addClass("scale-transform")
+    $(".base-bun").addClass("scale-transform")
+    $(".fries-group-01").addClass("scale-transform")
+    $(".fries-group-02").addClass("scale-transform")
+    $(".fries-back-container").addClass("scale-transform")
+    $(".fries-front-container").addClass("scale-transform")
+
+    $(".cup").addClass("scale-transform");
+   
+})
