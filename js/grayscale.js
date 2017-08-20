da5b5e27459ab378f52336a4c6b496e4eb75752f
apekearo <apekearo@gmail.com>
2017-08-20 17:43:05 -0500

    adding an uber eats link at the top and changed about text

--- js/grayscale.js
+++ js/grayscale.js
@@ -50,7 +50,7 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 17,
+        zoom: 16,
 
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(30.290256, -97.741947), // Crazy Ricky's
